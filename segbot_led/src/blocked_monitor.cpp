991bda2b283203aee2b70a62693a4ddd6a860af8
Nathan John <nj1407@gmail.com>
2017-04-27 09:57:32 -0500

    added back in a check for distance

--- segbot_led/src/blocked_monitor.cpp
+++ segbot_led/src/blocked_monitor.cpp
@@ -195,12 +195,13 @@ int main(int argc, char **argv)
             ROS_INFO_STREAM("old cou mnt " << old_count);
             ROS_INFO_STREAM("--------------------------pose size: " << old_size);
             ROS_INFO_STREAM("--------------------------pose size: " << current_path.poses.size());
-            while(((current_path.poses.size() < 5) && getDistance() > .5)  || (r_goal.status_list[0].status == 4) || (prevReplanCount + 30 < get_count_srv.response.replan_count))///
+            while(((current_path.poses.size() < 5) && getDistance() > .5)  || (r_goal.status_list[0].status == 4) || (prevReplanCount + 30 < get_count_srv.response.replan_count) || check >= 0)///
             {
                 //check = -check;
                 //ROS_INFO_STREAM("distanceToGoal " << distanceToGoal);
                 //ROS_INFO_STREAM("getDistance " << getDistance());
-                //ROS_INFO_STREAM("difference in loop " << (check));
+                ROS_INFO_STREAM("difference in loop " << (check));
+                ROS_INFO_STREAM("distanceToGoal " << distanceToGoal);
                 ROS_INFO_STREAM("in loop distance to goal " <<  getDistance());
                 ROS_INFO_STREAM("in of loop replan count " << get_count_srv.response.replan_count);
                 ROS_INFO_STREAM("prev of loop replan count " << prevReplanCount);
